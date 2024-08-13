96d66ded0dcb1c9e939e28cd6ce7849bf3a669c5
jwinkelmann <jwinkelmann@imply.com>
2024-08-13 20:22:58 -0300

    fix: Ajustando uso de env

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,9 +17,9 @@ dotenv.config();
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASS,
+      port: parseInt(process.env.DB_PORT || '5432'),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
       autoLoadEntities: true,
       synchronize: true,
