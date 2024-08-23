b9237522561073a423bb252f1c12b456e05a1a9a
jwinkelmann <jwinkelmann@imply.com>
2024-08-22 21:36:18 -0300

    fix:

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(process.env.PORT || 3000);
   app.useGlobalPipes(new ValidationPipe());
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
